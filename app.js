b29a3cc36f35007b35b2cead3229a99ef607137d
arails <arails.design@gmail.com>
2019-11-30 11:01:25 +0000

    Add nodemailer functionality as well as flash messages for success or failure of sending email.

--- app.js
+++ app.js
@@ -2,24 +2,40 @@ require("dotenv").config();
 const express       = require("express"),
     app           = express(),
     bodyParser    = require("body-parser"),
-    mongoose      = require("mongoose"),
+    cookieParser  = require("cookie-parser"),
     flash         = require("connect-flash"),
+	passport      = require("passport"),
     methodOverride = require("method-override");
 
 //requiring routes    
 var indexRoutes      = require("./routes/index");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-app.use(flash());
+
 
 app.locals.moment = require('moment');
+
+//COOKIE CONFIGURATION
+app.use(cookieParser());
+app.use(require("express-session")({
+    secret: process.env.PASSPORT_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+app.use(function(req, res, next){
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    next();
+});
 
 app.use("/", indexRoutes);
 
 
 app.listen(process.env.PORT, () => {
-	console.log('server is listening on port 3000');
+	console.log('Server started...');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-// var middleware = require("../middleware");
+var middleware = require("../middleware");
 var async = require("async");
 var nodemailer = require("nodemailer");
 var crypto = require("crypto");
@@ -8,6 +8,41 @@ var crypto = require("crypto");
 //root route
 router.get("/", function(req, res){
     res.render("index");
+});
+
+router.post("/send", function(req, res) {
+      var smtpTransport = nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+		port: 465,
+		secure: true, //use SSL
+        auth: {
+          user: 'arails.development@zohomail.com',
+          pass: process.env.ZOHOPW
+		}
+	  });
+	
+	var mailOptions = {
+        to: req.body.email,
+        from: 'arails.development@zohomail.com',
+        subject: 'Next Step',
+        text: 'Hello, \n\n' +
+		  'Thank you for visiting my portfolio site and expressing an interest in my work.\n\n' +
+          'Please respond to this e-mail with more information about the opportunity you have for me, and I will get back to you as soon as possible.\n\n' +
+		  'I look forward to communicating with you more in the near future! \n\n' +
+		  'Sincerely \n\n' +
+		  'Andrew Rails\n\n' +
+          'Note: If you did not request this email, please feel free to ignore or delete. Sorry for the inconvenience.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('success', 'Huzzah! An e-mail will be sent to ' + req.body.email + ' . Please check your spam folder if you don\'t see it in your inbox.' );
+        if (err){
+			console.log(err);
+			req.flash('error', 'Uh-oh, something went wrong...' );
+			return res.redirect("/");
+		} 
+		console.log('mail sent');
+		res.redirect("/");
+      });
 });
 
 module.exports = router;
